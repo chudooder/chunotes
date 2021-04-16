14b75fde44839f62c4ee65810ece212c7d1fac80
Chudooder <codeshawnachu@gmail.com>
2021-04-15 23:05:39 -0700

    Fix sidebar not having overflow

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 export function Sidebar(props: any) {
   return (
-    <div className="flex-none pt-2 bg-gray-800 text-white w-1/6">
+    <div className="flex-none pt-2 bg-gray-800 text-white w-1/6 display-block max-h-full h-full overflow-y-auto">
       <SidebarFolder depth={0} dir={props.files}/>
     </div>
   )
